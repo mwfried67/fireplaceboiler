807672fcdb8feb15f72198566cc7da04118717c0
mwfried67 <mwfried67@gmail.com>
2017-11-25 01:54:59 +0000

    fixed path in boilercontrol.sh

--- boilercontrol.sh
+++ boilercontrol.sh
@@ -2,4 +2,4 @@
 #cd /
 #sleep 10
 #cd home/pi/marcus_py
-sudo python /home/pi/marcus_py/boiler_control_p2/boilercontrol_v9.py
+sudo python /home/pi/Documents/clone_exp/fireplaceboiler/boilercontrol_v9.py
